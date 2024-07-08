cd72337c7588b6782aa51fd56ab685b87ff7b552
e.karapetyan <ek939944@gmail.com>
2024-07-09 01:54:42 +0300

    init commit

---
+++ homework/Chapter2.java
@@ -0,0 +1,42 @@
+package homework;
+
+public class Chapter2 {
+    public static void main(String[] args) {
+        int number;
+        number = 100;
+        System.out.println("The number equals " + number);
+        number = number * 2;
+        System.out.println("Number * 2 equals " + number);
+
+        System.out.println("-------------------");
+
+
+        int x, y;
+        x = 10;
+        y = 20;
+        if (x < y)
+            System.out.println("x<y");
+        x = x * 2;
+        if (x == y)
+            System.out.println("x==y");
+        x = x * 2;
+        if (x > y)
+            System.out.println("x>y");
+        if (x == y)
+            System.out.println();
+
+
+        int i;
+        for (i = 0; i < 10; i = i + 1)
+            System.out.println("i: " + i);
+
+        /* class BlockTest */
+        int a, b;
+        b = 20;
+        for (a = 0; a < 10; a++) {
+            System.out.println("a:" + a);
+            System.out.println("b:" + b);
+            b = b - 2;
+        }
+    }
+}
---
+++ homework/Chapter3.java
@@ -0,0 +1,49 @@
+package homework;
+
+public class Chapter3 {
+    /*Long class Light */
+
+    public static void main(String[] args) {
+
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+        lightspeed = 186000;
+        days = 1000;
+        seconds = days * 24 * 60 * 60;
+        distance = lightspeed * seconds;
+        System.out.print("  " + days);
+        System.out.print("         ");
+        System.out.println(distance + "  ");
+
+
+        double pi, r, a;
+        r = 10.8;
+        pi = 3.1416;
+        a = pi * r * r;
+        System.out.print("  " + a);
+
+        char ch1, ch2;
+        ch1 = 88;
+        ch2 = 'Y';
+        System.out.print(" ch1  ch2: ");
+        System.out.print(ch1 + "  " + ch2);
+        /* class CharDemo2 */
+        char ch3;
+        ch3 = 'X';
+        System.out.println(" ch3  " + ch3);
+        ch3++;
+        System.out.println(" ch3  " + ch3);
+        /* class boolTest */
+        boolean b;
+        b = false;
+        System.out.println(" b  " + b);
+        b = true;
+        System.out.println(" b  " + b);
+        if (b) System.out.println("  ");
+        b = false;
+        if (b) System.out.println("    ");
+        System.out.println(" 10 > 9  " + (10 > 9));
+    }
+}
